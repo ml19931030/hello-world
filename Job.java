8d36a98f4334a7e06be236dd969d832d7434b31b
ml19931030 <43953311+ml19931030@users.noreply.github.com>
2021-09-09 17:34:01 +0800

    Create Job.java

---
+++ Job.java
@@ -0,0 +1,29 @@
+package timeSum;
+
+public class Job {
+	private String jobId;
+	private String jobTimeS;
+	private String jobTimeE;
+	public String getJobId() {
+		return jobId;
+	}
+	public void setJobId(String jobId) {
+		this.jobId = jobId;
+	}
+	public String getJobTimeS() {
+		return jobTimeS;
+	}
+	public void setJobTimeS(String jobTimeS) {
+		this.jobTimeS = jobTimeS;
+	}
+	public String getJobTimeE() {
+		return jobTimeE;
+	}
+	public void setJobTimeE(String jobTimeE) {
+		this.jobTimeE = jobTimeE;
+	}
+
+
+
+
+}
